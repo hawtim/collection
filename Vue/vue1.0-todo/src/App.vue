1eabed868c1a8b205bf0c45d2e92e2d105339450
hawtim <hawtim_zhang@qq.com>
2017-11-14 10:34:43 +0800

    :fire: remove vue1.0 demo

--- Vue/vue1.0-todo/src/App.vue
+++ Vue/vue1.0-todo/src/App.vue
@@ -3,7 +3,7 @@
     <h1 v-html="title"></h1>
     <input v-model="newItem" v-on:keyup.enter="addNew">
     <ul>
-      <li v-for="item in items" v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)" >{{item.label}}</li>
+      <li v-for="item in items" v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)">{{item.label}}</li>
     </ul>
     <hello msg="hello props"></hello>
     <p>child tells me: {{ childWords }}</p>
@@ -18,38 +18,38 @@
   import ComponentA from './components/componentA'
   console.log(Store)
   export default {
-    data: function() {
-       return {
-         title:'this is father and this is a todo list',
-         items:Store.fetch(),
-         class1:'c1',
-         class2:'c2',
-         newItem:'',
-         childWords:''
-       }
+    data: function () {
+      return {
+        title: 'this is father and this is a todo list',
+        items: Store.fetch(),
+        class1: 'c1',
+        class2: 'c2',
+        newItem: '',
+        childWords: ''
+      }
     },
     components: {
       Hello,
       ComponentA,
     },
     events: {
-      "child-tell-me-something":function (msg) {
+      "child-tell-me-something": function (msg) {
         this.childWords = msg;
       }
     },
     methods: {
-      toggleFinish:function (item) {
+      toggleFinish: function (item) {
         item.isFinished = !item.isFinished
       },
       addNew: function () {
         this.items.push({
-          label:this.newItem,
+          label: this.newItem,
           isFinished: false
         })
-  //      console.log(this.newItem)
+        //      console.log(this.newItem)
         this.newItem = ""
         Store.save()
-        this.$broadcast('onAddnew',this.items)
+        this.$broadcast('onAddnew', this.items)
       },
       listenToMyBoy: function (msg) {
         this.childWords = msg;
@@ -57,47 +57,50 @@
 
     },
     watch: {
-      items:{
+      items: {
         handler: function (items) {
           console.log('father add ' + items)
           Store.save(items)
         },
-        deep:true
+        deep: true
       }
     }
-}
+  }
+
 </script>
 
 <style>
   .finished {
-    text-decoration:underline;
+    text-decoration: underline;
   }
-html {
-  height: 100%;
-}
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
+  html {
+    height: 100%;
+  }
 
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
+  body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
 
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
+  #app {
+    color: #2c3e50;
+    margin-top: -100px;
+    max-width: 600px;
+    font-family: Source Sans Pro, Helvetica, sans-serif;
+    text-align: center;
+  }
 
-.logo {
-  width: 100px;
-  height: 100px
-}
+  #app a {
+    color: #42b983;
+    text-decoration: none;
+  }
+
+  .logo {
+    width: 100px;
+    height: 100px
+  }
+
 </style>
