c5f52e71b1624987553c9aa5be87712436fe2233
hawtim <hawtim_zhang@qq.com>
2017-11-14 10:35:45 +0800

    :zap: improve utils

--- utils/index.js
+++ utils/index.js
@@ -1,23 +1,17 @@
 // 测试函数执行时间
-function test(func){
-
- var start = new Date().getTime() // 起始时间
-
- func() //执行待测函数
-
- var end = new Date().getTime() // 接受时间
-
- return (end - start)+"ms" // 返回函数执行需要时间
-
+function consoleTime(func) {
+  console.time('timer')
+  func()
+  console.timeEnd('timer')
 }
 
 // 简单的节流函数
-function debounce (func, wait) {
-    var timeout
-    return function () {
-        clearTimeout(timeout)
-        timeout = setTimeout(func, wait)
-    }
+function debounce(func, wait) {
+  var timeout
+  return function() {
+    clearTimeout(timeout)
+    timeout = setTimeout(func, wait)
+  }
 }
 
 // 数组去重
