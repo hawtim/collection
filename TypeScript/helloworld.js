60b2d51cf2a1784617abf2a2cf9f5c76ddf1c21a
hawtim <hawtim_zhang@qq.com>
2017-11-14 10:30:45 +0800

    :art: update typescript pratice

--- TypeScript/helloworld.js
+++ TypeScript/helloworld.js
@@ -1,14 +1,13 @@
-"use strict";
 // 多态与继承
-var Hello = (function () {
+var Hello = /** @class */ (function () {
     function Hello(fiestName, lastName) {
         this.firstName = fiestName;
         this.lastName = lastName;
     }
     Hello.prototype.greeter = function () {
-        return "hello! 欢迎来到typescript的世界，" + this.firstName + " " + this.lastName;
+        return 'hello! 欢迎来到typescript的世界，' + this.firstName + ' ' + this.lastName;
     };
     return Hello;
 }());
-var user = new Hello("zhang", "hawtim");
+var user = new Hello('zhang', 'hawtim');
 document.body.innerHTML = user.greeter();
--- TypeScript/helloworld.ts
+++ TypeScript/helloworld.ts
@@ -1,18 +1,18 @@
 // 多态与继承
 
 class Hello {
-    firstName : string;
-    lastName : string;
-    age: Number;
-    constructor(fiestName : string,  lastName : string) {
-        this.firstName = fiestName;
-        this.lastName = lastName;
-    }
-    greeter() {
-        return "hello! 欢迎来到typescript的世界，" + this.firstName + " " + this.lastName;
-    }
+  firstName: string
+  lastName: string
+  age: Number
+  constructor(fiestName: string, lastName: string) {
+    this.firstName = fiestName
+    this.lastName = lastName
+  }
+  greeter() {
+    return 'hello! 欢迎来到typescript的世界，' + this.firstName + ' ' + this.lastName
+  }
 }
 
-var user = new Hello("zhang", "hawtim");
+var user = new Hello('zhang', 'hawtim')
 
-document.body.innerHTML = user.greeter();
+document.body.innerHTML = user.greeter()
