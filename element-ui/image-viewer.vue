c12545b003310bda9abe6ee5929b00acc878a162
hawtim <hawtim_zhang@qq.com>
2018-04-02 15:32:15 +0800

    :zap: update elementUI components

---
+++ element-ui/image-viewer.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-dialog class="image-viewer" :visible.sync="dialogVisible" :show-close="false">
+        <img :src="imgUrl" alt="车牌照片">
+    </el-dialog>
+</template>
+<script>
+    export default {
+        props: {
+            dialogVisible: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            imgUrl: {
+                type: String,
+                required: true,
+                default: ''
+            }
+        }
+    }
+</script>
+<style>
+    .image-viewer {
+        background-color: #fff;
+    }
+</style>
+
