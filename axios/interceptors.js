564e08818608391e9e5fcd73d4f4c6a10c52376b
hawtim <hawtim_zhang@qq.com>
2018-04-19 20:47:37 +0800

    :zap: update interceptor

--- axios/interceptors.js
+++ axios/interceptors.js
@@ -1,30 +1,50 @@
-axios.interceptors.request.use(config => {
-    if (config.method == 'get') {
-        config.params = {
-            ...config.params,
-            _t: new Date().getTime()
-        }
-    }
-    return config
-}, err => {
-    return Promise.reject(err)
-})
+import axios from 'axios'
+import {Message} from 'element-ui'
+import router from '../router'
 
-axios.interceptors.response.use(res => {
-    return res.data.Code === 200 ? res : Promise.reject(res)
-}, err => {
-    if (!err.response) {
-        err.response = {
-            Code: 9990,
-            Message: '请求超时',
-            data: {}
-        }
-        return Promise.reject(err)
+export default {
+    install: function (V) {
+        axios.interceptors.request.use(config => {
+            if (config.method == 'get') {
+                config.params = {
+                    ...config.params,
+                    _t: new Date().getTime()
+                }
+            }
+            return config
+        }, err => {
+            return Promise.reject(err)
+        })
+
+        axios.interceptors.response.use(res => {
+            return res.data.Code === 200 ? res : Promise.reject(res)
+        }, err => {
+            if (!err.response) {
+                err.response = {
+                    Code: 9990,
+                    Message: '请求超时',
+                    data: {}
+                }
+                return Promise.reject(err)
+            }
+            let errorMessage = err.response.data.Message
+            if (error.response.status === 403) {
+                Message({
+                    type: error,
+                    message: '登录信息已过期，请重新登录！',
+                    duration: 3000
+                })
+                setTimeout(() => {
+                    // window.location.href = ''
+                }, 1000)
+                return Promise.reject(err)
+            }
+        })
+
+        V.$axios = axios
+        V.prototype.$axios = axios
     }
-    let errorMessage = err.response.data.Message
-    if (error.response.status === 'xxx') {
-        console.log('xxxx')
-        return Promise.reject(err)
-    }
-})
+}
+
+
 
